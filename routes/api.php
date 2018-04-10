457869334e3aade8eb47a340ae51d9eee0fca2bc
Samir Maikap <maikap.samir@gmail.com>
2018-04-10 11:36:27 +0530

    api routes documentation

--- routes/api.php
+++ routes/api.php
@@ -34,12 +34,12 @@ Route::group(['namespace' => 'API'], function () {
     Route::delete('accounts/{id}', 'UserController@delete');
 
     /*Services*/
-    Route::get('services/{id}', 'ServiceController@index');
-    Route::get('services/find/{id}', 'ServiceController@show');
-    Route::post('services/{id}', 'ServiceController@store');
-    Route::put('services/{id}', 'ServiceController@update');
-    Route::delete('services/{id}', 'ServiceController@delete');
-    Route::get('services/location/{location}', 'ServiceController@serviceByLocation');
-    Route::get('services/provider/{id}', 'ServiceController@serviceByProvider');
+    Route::get('services/{id}', 'ServiceController@index'); //service provider id
+    Route::get('services/find/{id}', 'ServiceController@show');//service id
+    Route::post('services/{id}', 'ServiceController@store');//service provider id
+    Route::put('services/{id}', 'ServiceController@update');//service id
+    Route::delete('services/{id}', 'ServiceController@delete');//service id
+    Route::get('services/location/{location}', 'ServiceController@serviceByLocation');//location
+    Route::get('services/provider/{id}', 'ServiceController@serviceByProvider');//service provider id
 
 });
